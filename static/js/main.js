70de65f515ea978f61c879f7ad22b5f700f13add
Joe Germuska <joe@germuska.com>
2013-05-31 12:17:29 -0500

    use more IDs and less classes and relativity

--- static/js/main.js
+++ static/js/main.js
@@ -7,8 +7,11 @@ SC.initialize({
     redirect_uri: "http://www.soundcite.com"
 });
 
-// Get the song
+function validate() {
+    return true;
+}
 
+// Get the song
 function load_sc_player() {
     var baseURL = $('#url').val();
     $.getJSON('https://soundcloud.com/oembed?callback=?', {
@@ -33,57 +36,57 @@ $("#url").keyup(function(event) { if(event.keyCode == 13) { load_sc_player();}})
 
 // Player functionality
 
-$(".start_btn").click(function() {
+$("#start_btn").click(function() {
+    var widget_iframe = $('#player_container').find('iframe');
+    var widget = SC.Widget(widget_iframe[0]);
+    widget.getPosition(function(position) {
+        $("#start_field").attr('value', Math.round(position));
+    });
+});
+
+$("#end_btn").click(function() {
     var widget_iframe = $('#player_container').find('iframe');
     var widget = SC.Widget(widget_iframe[0]);
     var clicked = $(this);
     widget.getPosition(function(position) {
-        clicked.prev('.start').attr('value', Math.round(position));
+        $("#end_field").attr('value', Math.round(position));
     });
 });
 
-$(".end_btn").click(function() {
-    var widget_iframe = $('#player_container').find('iframe');
-    var widget = SC.Widget(widget_iframe[0]);
-    var clicked = $(this);
-    widget.getPosition(function(position) {
-        clicked.prev('.end').attr('value', Math.round(position));
-    });
-});
+$('#button_wrapper').on("click", $('#create_clip'), function() {
+    if (validate()) {
+        var widget_iframe = $('#player_container').find('iframe');
+        var widget = SC.Widget(widget_iframe[0]);
+        start_time = $('#start_field').val();
+        end_time = $('#end_field').val();
+        var text = $('#linktext').val();
+        widget.getCurrentSound(function(currentSound) {
+            var elem = $('<span>').attr('data-id', currentSound.id).attr('data-start', start_time).attr('data-end', end_time).attr('class', 'soundcite').text(text);
+            var clip = new soundcite.Clip(elem[0]);
+            clips.push(clip)
+            SC.stream(clip.id, function(sound) {
+                sound.load({
+                    onload: function() {
+                        $('#audition_area').append("<div class='clip'>")
+                        $('.clip:last').append(clip.el);
+                        $('.clip:last').append("<input type='button' value='delete' class='btn btn-danger'>");
+                        $('.clip:last').append('<textarea readonly="readonly" class="code">&lt;span class="soundcite" data-id="' + clip.id + '" data-start="' + clip.start + '" data-end="' + clip.end + '"&gt;' + $(clip.el).text() + '&lt;/span&gt;</textarea>');
+                        $('#audition_area').append("</div>");
 
-$('#button_wrapper').on("click", $('.test_btn'), function() {
-    var widget_iframe = $('#player_container').find('iframe');
-    var widget = SC.Widget(widget_iframe[0]);
-    start_time = $('input').prev('.start').val();
-    end_time = $('input').prev('.end').val();
-    var text = $('#linktext').val();
-    widget.getCurrentSound(function(currentSound) {
-        var elem = $('<span>').attr('data-id', currentSound.id).attr('data-start', start_time).attr('data-end', end_time).attr('class', 'soundcite').text(text);
-        var clip = new soundcite.Clip(elem[0]);
-        clips.push(clip)
-        SC.stream(clip.id, function(sound) {
-            sound.load({
-                onload: function() {
-                    console.log('hi');
-                    $('#audition_area').append("<div class='clip'>")
-                    $('.clip:last').append(clip.el);
-                    $('.clip:last').append("<input type='button' value='delete' class='btn delete'>");
-                    $('.clip:last').append('<textarea readonly="readonly" class="code">&lt;span class="soundcite" data-id="' + clip.id + '" data-start="' + clip.start + '" data-end="' + clip.end + '"&gt;' + $(clip.el).text() + '&lt;/span&gt;</textarea>');
-                    $('#audition_area').append("</div>");
-
-                    $('.btn.delete').click(function() {
-                        var the_sound = $(this).prev('.soundcite');
-                        for (i=0; i<clips.length; i++) {
-                            if(the_sound[0] === clips[i].el) {
-                                clips.splice(i, 1);
+                        $('.btn.delete').click(function() {
+                            var the_sound = $(this).prev('.soundcite');
+                            for (i=0; i<clips.length; i++) {
+                                if(the_sound[0] === clips[i].el) {
+                                    clips.splice(i, 1);
+                                }
                             }
-                        }
-                        $(this).parents(".clip").remove();
-                    });
-                }
+                            $(this).parents(".clip").remove();
+                        });
+                    }
+                });
             });
         });
-    });
+    }
 })
 
 $('#embed_clips').click(function() {
@@ -97,7 +100,10 @@ $('#embed_clips').click(function() {
     $('#code').css('display', 'block');
 });
 
-// Presenting the code
+$("#example").click(function() {
+  $("#url").val($(this).text());
+  load_sc_player();
+});
 
 
 $('#header').click(function() {
